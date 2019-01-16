f0b86cbc78eb878f3309d9f5dd3af166c90977df
164169-vijay <vkuma236@DIN8004106.corp.capgemini.com>
2019-01-16 17:49:54 +0530

    "first commit"

---
+++ src/main/java/com/capgemini/Quikr/QuikrProject.java
@@ -0,0 +1,13 @@
+package com.capgemini.Quikr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuikrProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuikrProject.class, args);
+	}
+
+}
