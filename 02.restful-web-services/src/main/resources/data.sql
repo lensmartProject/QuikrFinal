0aab533a612090fbd9cf066c006a5cd7469a5a97
prabhat kumar <45679073+lensmartProject@users.noreply.github.com>
2020-06-22 00:03:50 +0530

    newew
    new

---
+++ 02.restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(10001, sysdate(), 'Virat');
+insert into user values(10002, sysdate(), 'Dhoni');
+insert into user values(10003, sysdate(), 'Yuvraj');
+insert into post values(11001, 'My First Post', 10001);
+insert into post values(11002, 'My Second Post', 10001);
